b7d3ae69c353705449808bd72bc0e8fe2ce49e9c
Petr Arapov <parapov@datanomica.org>
2024-01-21 11:35:44 +0300

    Homework2_1 done

---
+++ src/main/java/ru/inno/course/Homework2_1.java
@@ -0,0 +1,30 @@
+package ru.inno.course;
+
+public class Homework2_1 {
+
+    public static void main(String[] args) {
+
+        // Присваиваем переменной cardNumber значение
+        String cardNumber = "1234 5678 9012 3456";
+
+        // Убираем все пробелы из строки, чтобы строка всегда была в одном виде
+        cardNumber = cardNumber.replaceAll(" ", "");
+
+        // Считаем длину строки
+        int сardNumberLength = cardNumber.length();
+
+        // Создаем строку, которая будет содержать нужное кол-во звездочек для replace
+        String starsForReplace = "*".repeat(сardNumberLength - 4);
+
+        // Заменяем первые сardNumberLength - 4 символы на звездочки
+        String hiddenCardNumber = starsForReplace + cardNumber.substring(сardNumberLength - 4);
+
+        // Наводим красоту - ставим пробелы через каждые четыре символа
+        String cardNumberToShow = hiddenCardNumber.replaceAll("(.{4})", "$1 ");
+
+        System.out.println(cardNumberToShow);
+
+    }
+
+}
+
