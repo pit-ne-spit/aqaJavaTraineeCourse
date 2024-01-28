acafa5a50d96931b43f4909f659a61d12a9944a5
Petr Arapov <parapov@datanomica.org>
2024-01-28 12:51:22 +0300

    Homework3_4 done

---
+++ src/main/java/ru/inno/course/Homework3_4.java
@@ -0,0 +1,10 @@
+package ru.inno.course;
+
+public class Homework3_4 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 1000000000; i++) {
+            System.out.println(i);
+
+        }
+    }
+}
