91d0a419b40d7be8d4b5c450a4cae64c7ced94da
Petr Arapov <parapov@datanomica.org>
2024-01-28 13:42:14 +0300

    homework4_2 done

---
+++ src/main/java/ru/inno/course/Homework4_2.java
@@ -0,0 +1,14 @@
+package ru.inno.course;
+
+public class Homework4_2 {
+    public static void main(String[] args) {
+        int number = 101;
+
+        if (number%2 != 0) {
+            System.out.println("Нечетное число");
+        }
+        else {
+            System.out.println("Четное число");
+        }
+    }
+}
