5be5428dfe85b2be518a598688d5b7d6aafa088b
Petr Arapov <parapov@datanomica.org>
2024-01-28 13:34:05 +0300

    homework4_1 done

---
+++ src/main/java/ru/inno/course/Homework4_1.java
@@ -0,0 +1,13 @@
+package ru.inno.course;
+
+public class Homework4_1 {
+    public static void main(String[] args) {
+        int balance = 10;
+        if (balance == 10) {
+            System.out.println("Десятка");
+        }
+        else {
+            System.out.println("Не десятка");
+        }
+    }
+}
--- src/main/java/ru/inno/course/arapovPetr.java
+++
@@ -1,9 +0,0 @@
-package ru.inno.course;
-
-public class arapovPetr {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
-}
-
-//
