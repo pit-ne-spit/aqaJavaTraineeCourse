ce9a6bf53b77726c4c1e05b4e93906f393f0467b
Petr Arapov <parapov@datanomica.org>
2024-01-28 12:05:41 +0300

    Homework3_2 done

---
+++ src/main/java/ru/inno/course/Homework3_2.java
@@ -0,0 +1,15 @@
+package ru.inno.course;
+
+public class Homework3_2 {
+    public static void main(String[] args) {
+        double[] piE1 = new double[3];
+        piE1[0] = 3.14159;
+        piE1[1] = 2.71828;
+        piE1[2] = 1;
+
+        for (double element: piE1) {
+            System.out.println(element);
+
+        }
+    }
+}
