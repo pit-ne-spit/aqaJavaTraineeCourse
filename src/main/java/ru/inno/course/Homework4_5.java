d6154528fe6a54420e9a712f121fd17db6c4a861
Petr Arapov <parapov@datanomica.org>
2024-01-28 16:06:58 +0300

    homework4_5 done

---
+++ src/main/java/ru/inno/course/Homework4_5.java
@@ -0,0 +1,36 @@
+package ru.inno.course;
+
+public class Homework4_5 {
+    public static void main(String[] args) {
+        String password = "qwqeqweqwy1!h";
+        boolean containsDigit = false;
+        boolean containsSymbol = false;
+        String[] symbols = {"!", ":", "#", "$", "%", "^", "&", "*", "№"};
+
+
+        if (password.length() >= 8) {
+            for (char element : password.toCharArray()) {
+                if (Character.isDigit(element)) {
+                    containsDigit = true;
+                    break;
+                }
+            }
+            if (containsDigit == true) {
+                for (char element : password.toCharArray()) {
+                    for (String symbol: symbols) {
+                        if (symbol.equals(String.valueOf(element))) {
+                            containsSymbol = true;
+                            break;
+                        }
+                    }
+                }
+                if (containsSymbol == true) {
+                    System.out.println("Пароль принят");
+                }
+                else System.out.println("Пароль должен содержать минимум 1 спецсимвол");
+            }
+            else System.out.println("Пароль должен содержать минимум 1 цифру");
+        }
+        else System.out.println("Пароль должен быть не менее 8 символов");
+    }
+}
