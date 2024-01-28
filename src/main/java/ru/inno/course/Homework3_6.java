511776c44607064ae6cf72ed4e03b0daea34fcd6
Petr Arapov <parapov@datanomica.org>
2024-01-28 13:07:31 +0300

    Homework3_6 done

---
+++ src/main/java/ru/inno/course/Homework3_6.java
@@ -0,0 +1,14 @@
+package ru.inno.course;
+
+public class Homework3_6 {
+    public static void main(String[] args) {
+
+        String[] players = {"Ken", "Ben", "Ruben", "Stan", "Vladlen", "Madlen", "Colen", "Tyumen", "Muren", "Rahmatullo"};
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println(players[i]);
+
+        }
+
+    }
+}
