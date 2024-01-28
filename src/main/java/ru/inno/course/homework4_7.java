beb9e8f14ac2915d7a4a24123561913008da4091
Petr Arapov <parapov@datanomica.org>
2024-01-28 21:40:43 +0300

    homework4_7 done

---
+++ src/main/java/ru/inno/course/homework4_7.java
@@ -0,0 +1,20 @@
+package ru.inno.course;
+
+public class homework4_7 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("Fizz Buzz");
+                continue;
+            }
+            if (i % 3 == 0) {
+                System.out.println("Fizz");
+                continue;
+            }
+            if (i % 5 == 0) {
+                System.out.println("Buzz");
+            }
+            else System.out.println(i);
+        }
+    }
+}
